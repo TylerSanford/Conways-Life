4c3540ac393b6cb8cc6090052572278795b7b899
Tyler Sanford <tylersanford0311@gmail.com>
2018-03-01 17:18:46 -0600

    stretch buttons added

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -2,115 +2,177 @@ import React, { Component } from 'react';
 import Life from './life';
 import './App.css';
 
+import { Button, Jumbotron } from 'reactstrap';
+import { Card } from 'reactstrap';
+
 /**
  * Life canvas
  */
 class LifeCanvas extends Component {
-
-  /**
-   * Constructor
-   */
-  constructor(props) {
-    super(props);
-
-    this.life = new Life(props.width, props.height);
-    this.life.randomize();
-  }
-
-  /**
-   * Component did mount
-   */
-  componentDidMount() {
-    requestAnimationFrame(() => {this.animFrame()});
-  }
-
-  /**
-   * Handle an animation frame
-   */
-  animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
-    let cells = this.life.getCells();
-    let width = this.props.width;
-    let height = this.props.height;
-
-    // Request another animation frame
-    // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
-
-    // Get canvas framebuffer, a packed RGBA array
-    const canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    let imageData = ctx.getImageData(0, 0, width, height);
-
-    // Updated the imageData based on the cells
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const state = cells[y][x];
-        const color = state === 0 ? 0x00 : 0xff;
-        const index = (y * width + x) * 4;
-
-        imageData.data[index + 0] = color;
-        imageData.data[index + 1] = color;
-        imageData.data[index + 2] = color;
-        imageData.data[index + 3] = 0xff;
-      }
+    animate = true;
+    /**
+     * Constructor
+     */
+    constructor(props) {
+        super(props);
+        this.life = new Life(props.width, props.height);
+        this.life.randomize();
     }
 
-    // put the new image data back on the canvas
-    ctx.putImageData(imageData, 0, 0);
+    /**
+     * Component did mount
+     */
+    componentDidMount() {
+        requestAnimationFrame(() => {
+            this.animFrame();
+        });
+    }
 
-    // Iterate the game state!
-    this.life.step();
+    /**
+     * Handle an animation frame
+     */
+    animFrame() {
+        //
+        // !!!! IMPLEMENT ME !!!!
+        //
+        let cells = this.life.getCells();
+        let width = this.props.width;
+        let height = this.props.height;
 
-    // Request another animation frame
-    requestAnimationFrame(() => {this.animFrame()});
-  }
+        // Request another animation frame
+        // Update life and get cells
+        // Get canvas framebuffer, a packed RGBA array
+        // Convert the cell values into white or black for the canvas
+        // Put the new image data back on the canvas
+        // Next generation of life
 
-  /**
-   * Render
-   */
-  render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
-  }
+        // Get canvas framebuffer, a packed RGBA array
+        const canvas = this.refs.canvas;
+        let ctx = canvas.getContext('2d');
+        canvas.style.width = canvas.width * 2 + 'px';
+        canvas.style.height = canvas.height * 2 + 'px';
+        let imageData = ctx.getImageData(0, 0, width, height);
+
+        // Updated the imageData based on the cells
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const state = cells[y][x];
+                const color = state === 0 ? 0xff : 0x00;
+                const index = (y * width + x) * 4;
+
+                imageData.data[index + 0] = color;
+                imageData.data[index + 1] = color;
+                imageData.data[index + 2] = color;
+                imageData.data[index + 3] = 100;
+            }
+        }
+
+        // put the new image data back on the canvas
+        ctx.putImageData(imageData, 0, 0);
+
+        // Iterate the game state!
+        if (this.animate) {
+            this.life.step();
+        }
+
+        // Request another animation frame
+        requestAnimationFrame(() => {
+            this.animFrame();
+        });
+    }
+
+    handleButtonClickNewCanvas() {
+        this.animate = true;
+        this.life.randomize();
+    }
+
+    handleButtonClickStop() {
+        this.animate = false;
+    }
+
+    handleButtonClickStart() {
+        this.animate = true;
+    }
+
+    handleButtonClickClear() {
+        this.life.clear();
+    }
+
+    /**
+     * Render
+     */
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <h1>The Game of Life</h1>
+                    <canvas
+                        ref="canvas"
+                        width={this.props.width}
+                        height={this.props.height}
+                    />
+                    <br />
+                    <Button
+                        color="primary"
+                        onClick={this.handleButtonClickNewCanvas.bind(this)}
+                    >
+                        New Canvas
+                    </Button>
+                    <Button
+                        color="danger"
+                        onClick={this.handleButtonClickStop.bind(this)}
+                    >
+                        Stop
+                    </Button>
+                    <Button
+                        color="success"
+                        onClick={this.handleButtonClickStart.bind(this)}
+                    >
+                        Start
+                    </Button>
+                    <Button
+                        color="warning"
+                        onClick={this.handleButtonClickClear.bind(this)}
+                    >
+                        Clear Canvas
+                    </Button>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 /**
  * Life holder component
  */
 class LifeApp extends Component {
+    /**
+     * Render
+     */
 
-  /**
-   * Render
-   */
-  render() {
-    return (
-      <div>
-        <LifeCanvas width={400} height={300} />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <LifeCanvas width={200} height={200} />
+            </div>
+        );
+    }
 }
 
 /**
  * Outer App component
  */
 class App extends Component {
-
-  /**
-   * Render
-   */
-  render() {
-    return (
-      <div className="App">
-        <LifeApp />
-      </div>
-    );
-  }
+    /**
+     * Render
+     */
+    render() {
+        return (
+            <div className="App">
+                <LifeApp />
+            </div>
+        );
+    }
 }
 
 export default App;
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -6,171 +6,165 @@
  * Make a 2D array helper function
  */
 function Array2D(width, height) {
-  let a = new Array(height);
-  for (let i = 0; i < height; i++) {
-    a[i] = new Array(width);
-  }
+    let a = new Array(height);
+    for (let i = 0; i < height; i++) {
+        a[i] = new Array(width);
+    }
 
-  return a;
+    return a;
 }
 
 /**
  * Life class
  */
 class Life {
+    /**
+     * Constructor
+     */
+    constructor(width, height) {
+        // !!!! IMPLEMENT ME !!!!
+        this.width = width;
+        this.height = height;
 
-  /**
-   * Constructor
-   */
-  constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
-    this.width = width;
-    this.height = height;
+        this.currentBufferIndex = 0;
+        this.buffer = [Array2D(width, height), Array2D(width, height)];
 
-    this.currentBufferIndex = 0;
-    this.buffer = [
-      Array2D(width, height),
-      Array2D(width, height)
-    ];
-
-    this.clear();
-  }
-  
-  /**
-   * Return the current active buffer
-   * 
-   * This should NOT be modified by the caller
-   */
-  getCells() {
-    // !!!! IMPLEMENT ME !!!!
-    return this.buffer[this.currentBufferIndex];
-  }
-
-  /**
-   * Clear the life grid
-   */
-  clear() {
-    // !!!! IMPLEMENT ME !!!!
-    for (let y = 0; y < this.height; y++) {
-      this.buffer[this.currentBufferIndex][y].fill(0);
-    }
-  }
-  
-  /**
-   * Randomize the life grid
-   */
-  randomize() {
-    // !!!! IMPLEMENT ME !!!!
-    for (let y = 0; y < this.height; y++) {
-      for (let x = 0; x < this.width; x++) {
-        const rand = (Math.random()*2)|0;
-        this.buffer[this.currentBufferIndex][y][x] = rand;
-      }
-    }
-  }
-
-  /**
-   * Run the simulation for a single step
-   */
-  step() {
-    // !!!! IMPLEMENT ME !!!!
-    // Fill the offscreen buffer with the next life generation
-    // from the current buffer.
-
-    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
-    let currentBuffer = this.buffer[this.currentBufferIndex];
-    let backBuffer = this.buffer[backBufferIndex];
-
-     // See if we have a neighbor to infect this one
-     function hasInfectiousNeighbor(x, y) {
-      const nextValue = (currentBuffer[y][x] + 1) % 2;
-      let neighborCount = 0;
-
-      // North West -  neighbor of cell x, y
-      if (y > 0 && x > 0) {
-        if (currentBuffer[y - 1][x - 1] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // North -  neighbor of cell x, y
-      if (y > 0) {
-        if (currentBuffer[y - 1][x] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // North East -  neighbor of cell x, y
-      if (y > 0 && y < this.width - 1) {
-        if (currentBuffer[y - 1][x + 1] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // East -  neighbor of cell x, y
-      if (y < this.width - 1) {
-        if (currentBuffer[y][x + 1] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // South East -  neighbor of cell x, y
-      if (y < this.height - 1 && y < this.width - 1) {
-        if (currentBuffer[y + 1][x + 1] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // South -  neighbor of cell x, y
-      if (y < this.height - 1) {
-        if (currentBuffer[y + 1][x] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // South West -  neighbor of cell x, y
-      if (y < this.height - 1 && x > 0) {
-        if (currentBuffer[y + 1][x - 1] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // WEST -  neighbor of cell x, y
-      if (x > 0) {
-        if (currentBuffer[y][x - 1] === nextValue) {
-          neighborCount++;
-        }
-      }
-
-      // If we've made it this far we aren't infected
-      return neighborCount;
+        this.clear();
     }
 
-    // Loop through and decide the state of the next generation
-    // for each cell processed.
-    for (let y = 0; y < this.height; y++) {
-      for (let x = 0; x < this.width; x++) {
-        // let count = hasInfectiousNeighbor.call(this, x, y);
-        let count = (hasInfectiousNeighbor.bind(this))(x, y);
-        let current = currentBuffer[y][x];
-
-        if (current === 1) {
-          if (count < 2 || count > 3) {
-            backBuffer[y][x] = 0;
-          } else {
-            backBuffer[y][x] = 1;
-          }
-        } else {
-          if (count === 3) {
-            backBuffer[y][x] = 1;
-          } else {
-            backBuffer[y][x] = 0;
-          }
-        }
-      }
+    /**
+     * Return the current active buffer
+     *
+     * This should NOT be modified by the caller
+     */
+    getCells() {
+        // !!!! IMPLEMENT ME !!!!
+        return this.buffer[this.currentBufferIndex];
     }
-    this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
-  }
+
+    /**
+     * Clear the life grid
+     */
+    clear() {
+        // !!!! IMPLEMENT ME !!!!
+        for (let y = 0; y < this.height; y++) {
+            this.buffer[this.currentBufferIndex][y].fill(0);
+        }
+    }
+
+    /**
+     * Randomize the life grid
+     */
+    randomize() {
+        // !!!! IMPLEMENT ME !!!!
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                const rand = (Math.random() * 2) | 0;
+                this.buffer[this.currentBufferIndex][y][x] = rand;
+            }
+        }
+    }
+
+    /**
+     * Run the simulation for a single step
+     */
+    step() {
+        // !!!! IMPLEMENT ME !!!!
+        // Fill the offscreen buffer with the next life generation
+        // from the current buffer.
+
+        let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+        let currentBuffer = this.buffer[this.currentBufferIndex];
+        let backBuffer = this.buffer[backBufferIndex];
+
+        // See if we have a neighbor to infect this one
+        function hasInfectiousNeighbor(x, y) {
+            let neighborCount = 0;
+
+            // North West - neighbor of cell x, y
+            if (y > 0 && x > 0) {
+                if (currentBuffer[y - 1][x - 1]) {
+                    neighborCount++;
+                }
+            }
+
+            // North -  neighbor of cell x, y
+            if (y > 0) {
+                if (currentBuffer[y - 1][x]) {
+                    neighborCount++;
+                }
+            }
+
+            // North East - neighbor of cell x, y
+            if (y > 0 && y < this.width - 1) {
+                if (currentBuffer[y - 1][x + 1]) {
+                    neighborCount++;
+                }
+            }
+
+            // East - neighbor of cell x, y
+            if (y < this.width - 1) {
+                if (currentBuffer[y][x + 1]) {
+                    neighborCount++;
+                }
+            }
+
+            // South East - neighbor of cell x, y
+            if (y < this.height - 1 && y < this.width - 1) {
+                if (currentBuffer[y + 1][x + 1]) {
+                    neighborCount++;
+                }
+            }
+
+            // South -  neighbor of cell x, y
+            if (y < this.height - 1) {
+                if (currentBuffer[y + 1][x]) {
+                    neighborCount++;
+                }
+            }
+
+            // South West - neighbor of cell x, y
+            if (y < this.height - 1 && x > 0) {
+                if (currentBuffer[y + 1][x - 1]) {
+                    neighborCount++;
+                }
+            }
+
+            // WEST - neighbor of cell x, y
+            if (x > 0) {
+                if (currentBuffer[y][x - 1]) {
+                    neighborCount++;
+                }
+            }
+
+            // If we've made it this far we aren't infected
+            return neighborCount;
+        }
+
+        // Loop through and decide the state of the next generation
+        // for each cell processed.
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                let count = hasInfectiousNeighbor.call(this, x, y);
+                let current = currentBuffer[y][x];
+
+                if (current === 1) {
+                    if (count < 2 || count > 3) {
+                        backBuffer[y][x] = 0;
+                    } else {
+                        backBuffer[y][x] = 1;
+                    }
+                } else {
+                    if (count === 3) {
+                        backBuffer[y][x] = 1;
+                    } else {
+                        backBuffer[y][x] = 0;
+                    }
+                }
+            }
+        }
+        this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    }
 }
 
 export default Life;
